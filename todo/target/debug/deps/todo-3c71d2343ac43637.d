d689b19983767d3bf5bf28d312d3e2c80d50e8fe
sincerelyyyash <yta0088@gmail.com>
2025-02-17 23:42:16 +0530

    Basic todo functions

---
+++ todo/target/debug/deps/todo-3c71d2343ac43637.d
@@ -0,0 +1,9 @@
+/Users/yash/DevProjects/rust/learning-rust/todo/target/debug/deps/libtodo-3c71d2343ac43637.rmeta: src/main.rs Cargo.toml
+
+/Users/yash/DevProjects/rust/learning-rust/todo/target/debug/deps/todo-3c71d2343ac43637.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
---
+++ todo/target/debug/deps/todo-babadb6724eef197.d
@@ -0,0 +1,9 @@
+/Users/yash/DevProjects/rust/learning-rust/todo/target/debug/deps/libtodo-babadb6724eef197.rmeta: src/main.rs Cargo.toml
+
+/Users/yash/DevProjects/rust/learning-rust/todo/target/debug/deps/todo-babadb6724eef197.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
